802796efc47225625c6e8d4c6c7729dc7d08e8ac
Mateusz <mateuszturek005@gmail.com>
2020-03-09 14:59:37 +0100

    Initial commit, created test for HomeController class

---
+++ src/test/java/com/mat/pizza/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.mat.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+@RunWith(SpringRunner.class)
+class HomeControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    public HomeControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void testHome() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Welcome"))
+                .andExpect(content().string(containsString("Welcome to Mat Pizza")));
+    }
+}
